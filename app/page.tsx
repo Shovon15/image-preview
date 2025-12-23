9598f3397eacd1524a568fa26953b83c1507a7a2
shovon15 <shovonrnbd@gmail.com>
2025-12-23 11:57:12 +0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,99 @@
 import Image from "next/image";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Social Share Preview",
+  description: "This is a social share preview.",
+  openGraph: {
+    title: "Social Share Preview",
+    description: "This is a social share preview.",
+    images: [
+      {
+        url: "/assets/city_lights.jpg",
+        width: 1200,
+        height: 630,
+        alt: "city_lights",
+      },
+    ],
+  },
+};
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="flex flex-col items-center gap-10 px-10">
+      <p className="text-4xl font-bold text-center pt-10">Hello world!</p>
+      <Image
+        src="/assets/city_lights.jpg"
+        alt="blog"
+        width={1200}
+        height={1200}
+      />
+      <p className="pb-10">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas quis rem
+        inventore beatae aut culpa, esse accusantium sint rerum molestias optio
+        eius, at et autem impedit necessitatibus iure nostrum voluptatum saepe
+        deleniti. Dignissimos quod ut non unde, molestias, cupiditate temporibus
+        error, pariatur vel quos atque voluptate eveniet. Corporis laudantium ab
+        distinctio quos, delectus quis itaque molestiae ipsam quod provident
+        modi quisquam nulla illum dolorum assumenda porro consequuntur nihil! Ea
+        dolorum quasi, illum exercitationem placeat iste obcaecati aut
+        repudiandae culpa a debitis laboriosam officia, doloremque incidunt
+        provident. Possimus, voluptatum. Sapiente, error ipsum numquam sit quasi
+        aliquid quis magnam eligendi ullam. Optio numquam labore magni accusamus
+        perspiciatis officiis laboriosam, quo eos omnis aut dolorem earum libero
+        excepturi tempora placeat sint architecto, enim odit repellat fugiat
+        nihil vel? Non, praesentium commodi! Vitae mollitia tenetur eius. Iste,
+        tempore nobis. Accusantium quisquam ea fugit magnam est itaque culpa
+        praesentium cum ad dolore. Labore hic soluta vero, quod quaerat dolore
+        culpa. Velit animi veniam enim, quae esse illum sequi porro? Nisi
+        distinctio deserunt, quia ratione accusantium unde atque sint neque
+        consequuntur nulla numquam natus voluptate nam quo. Natus, et!
+        Distinctio fugiat sunt odio recusandae doloribus iusto dolores corrupti
+        maiores blanditiis! Cumque a dolor fuga doloribus odio ab debitis,
+        aspernatur sapiente nostrum id, fugit ipsum eligendi totam amet iure
+        saepe at. Rerum porro id quod corrupti ullam mollitia libero omnis,
+        voluptate doloribus accusantium, sint obcaecati deserunt accusamus dicta
+        tempora nostrum hic modi placeat sequi reiciendis dolore a possimus
+        fugiat. Ratione accusantium atque repellat, porro nulla sed voluptate
+        numquam repellendus facilis, modi pariatur consectetur deserunt tempore.
+        Alias perferendis quibusdam itaque harum, ad illum libero repellat
+        quidem quam. Nulla dolore magnam facere, corporis consequatur tenetur
+        fuga magni, quidem autem veniam deleniti rerum neque labore ipsum. Et
+        dicta vero molestiae nisi corporis. Neque, voluptatem voluptates beatae
+        aliquam natus ipsa praesentium! <br /> <br /> Lorem ipsum dolor, sit
+        amet consectetur adipisicing elit. Quas quis rem inventore beatae aut
+        culpa, esse accusantium sint rerum molestias optio eius, at et autem
+        impedit necessitatibus iure nostrum voluptatum saepe deleniti.
+        Dignissimos quod ut non unde, molestias, cupiditate temporibus error,
+        pariatur vel quos atque voluptate eveniet. Corporis laudantium ab
+        distinctio quos, delectus quis itaque molestiae ipsam quod provident
+        modi quisquam nulla illum dolorum assumenda porro consequuntur nihil! Ea
+        dolorum quasi, illum exercitationem placeat iste obcaecati aut
+        repudiandae culpa a debitis laboriosam officia, doloremque incidunt
+        provident. Possimus, voluptatum. Sapiente, error ipsum numquam sit quasi
+        aliquid quis magnam eligendi ullam. Optio numquam labore magni accusamus
+        perspiciatis officiis laboriosam, quo eos omnis aut dolorem earum libero
+        excepturi tempora placeat sint architecto, enim odit repellat fugiat
+        nihil vel? Non, praesentium commodi! Vitae mollitia tenetur eius. Iste,
+        tempore nobis. Accusantium quisquam ea fugit magnam est itaque culpa
+        praesentium cum ad dolore. Labore hic soluta vero, quod quaerat dolore
+        culpa. Velit animi veniam enim, quae esse illum sequi porro? Nisi
+        distinctio deserunt, quia ratione accusantium unde atque sint neque
+        consequuntur nulla numquam natus voluptate nam quo. Natus, et!
+        Distinctio fugiat sunt odio recusandae doloribus iusto dolores corrupti
+        maiores blanditiis! Cumque a dolor fuga doloribus odio ab debitis,
+        aspernatur sapiente nostrum id, fugit ipsum eligendi totam amet iure
+        saepe at. Rerum porro id quod corrupti ullam mollitia libero omnis,
+        voluptate doloribus accusantium, sint obcaecati deserunt accusamus dicta
+        tempora nostrum hic modi placeat sequi reiciendis dolore a possimus
+        fugiat. Ratione accusantium atque repellat, porro nulla sed voluptate
+        numquam repellendus facilis, modi pariatur consectetur deserunt tempore.
+        Alias perferendis quibusdam itaque harum, ad illum libero repellat
+        quidem quam. Nulla dolore magnam facere, corporis consequatur tenetur
+        fuga magni, quidem autem veniam deleniti rerum neque labore ipsum. Et
+        dicta vero molestiae nisi corporis. Neque, voluptatem voluptates beatae
+        aliquam natus ipsa praesentium!
+      </p>
     </div>
   );
 }
